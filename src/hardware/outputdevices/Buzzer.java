2c12651b32847b1ab906c3e2b67ea085d5c7e3c3
simonavans <saj.decock@student.avans.nl>
2023-01-02 13:02:44 +0100

    Make Buzzer of type Updatable

--- src/hardware/outputdevices/Buzzer.java
+++ src/hardware/outputdevices/Buzzer.java
@@ -3,16 +3,19 @@ package hardware.outputdevices;
 import TI.PWM;
 import TI.Timer;
 import hardware.PinRegistry;
+import link.Updatable;
 
 /**
  * Class for a buzzer on the BoeBot.
+ *
  * @author Simon de Cock
  */
-public class Buzzer {
+public class Buzzer implements Updatable {
     private PWM pwm;
     private Timer repeatingBeepMethodTimer;
     private Timer repeatingBeepTimer;
     private boolean beepState;
+    private int beepDuration = 0;
 
     public Buzzer(byte pinNumber) {
         PinRegistry.registerPins(new int[]{pinNumber}, new String[]{"output"});
@@ -21,35 +24,46 @@ public class Buzzer {
         pwm = new PWM(pinNumber, 0);
     }
 
-    /**
-     * Makes the buzzer beep repeatingly and asynchronously using the Timer class. In other
-     * words, this method does not block the run method in RobotMain when called. Rather,
-     * this method relies on being continuously called in the run method to check when it
-     * should beep or not.
-     * @param milliseconds How long a full cycle (on and off time combined) should last.
-     * @author Simon de Cock
-     */
-    public void repeatingBeep(int milliseconds) {
-        if (repeatingBeepMethodTimer == null) {
-            repeatingBeepMethodTimer = new Timer(milliseconds);
+    @Override
+    public void update() {
+        if (repeatingBeepMethodTimer == null) return;
+
+        if (repeatingBeepMethodTimer.timeout()) {
+            repeatingBeepMethodTimer = null;
+            repeatingBeepTimer = null;
+            beepDuration = 0;
+            return;
         }
 
-        if (!repeatingBeepMethodTimer.timeout()) {
-            if (repeatingBeepTimer == null) {
-                repeatingBeepTimer = new Timer(milliseconds / 2);
-            }
+        if (repeatingBeepTimer == null) {
+            repeatingBeepTimer = new Timer(beepDuration);
+            repeatingBeepTimer.mark();
+        }
 
-            if (repeatingBeepTimer.timeout()) {
-                beepState = !beepState;
+        if (repeatingBeepTimer.timeout()) {
+            beepState = !beepState;
+            pwm.update(beepState ? 128 : 0);
+        }
+    }
 
-                if (beepState) {
-                    pwm.update(128);
-                } else {
-                    pwm.update(0);
-                }
-            }
+    /**
+     * Makes the buzzer beep repeatingly and asynchronously using the Timer class. In other
+     * words, this method does not block the run method in RobotMain when called. Since Buzzer
+     * is Updatable, which means that calling this method once will run repeatingBeep until
+     * sufficient time has passed. Calling this method while the buzzer is already beeping on
+     * repeat will override the previous repeating beep.
+     *
+     * @param milliseconds How long a full cycle (on and off time combined) should last.
+     * @param times How many times the buzzer should beep in the given time frame.
+     * @author Simon de Cock
+     */
+    public void repeatingBeep(int milliseconds, int times) {
+        if (milliseconds > 1) {
+            beepDuration = milliseconds / (times * 2);
+            repeatingBeepMethodTimer = new Timer(milliseconds);
+            repeatingBeepMethodTimer.mark();
         } else {
-            repeatingBeepMethodTimer = null;
+            System.out.println("Warning: parameter milliseconds for repeatingBeep method must be greater than 1");
         }
     }
 }
